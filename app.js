b4ed53a872a7df917e62e1ef3a2b70c3df188c59
Jorge <cinfacol@gmail.com>
2021-07-08 20:35:32 -0500

    Audio List Player

---
+++ app.js
@@ -0,0 +1,10 @@
+
+import AudioPlayer from './AudioPlayer.js';
+
+const player = new AudioPlayer('.audioPlayer', [
+    { name: 'Dance Monkey', url: 'media/audios/Dance_Monkey.mp3' },
+    { name: 'Memories', url: 'media/audios/Memories.mp3' },
+    { name: 'Girls Like You', url: 'media/audios/Girls_Like_You.mp3' },
+    { name: 'Señorita', url: 'media/audios/Senorita.mp3' },
+    { name: 'On My Way', url: 'media/audios/On_My_Way.mp3' },
+]);
